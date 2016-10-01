5aa2b01e03a25788b6e968bd44f0ecab8fdc69bb
eligantRU <eligant.ru@gmail.com>
2016-10-01 19:24:31 +0400

    issue/4

--- lw1/replace/main.cpp
+++ lw1/replace/main.cpp
@@ -19,37 +19,18 @@ std::string FindAndReplace(const std::string & inputStr, const std::string & sea
 	return result;
 }
 
-}
-
-int main(int argc, char * argv[])
+void Replace(const std::string & inputFilePath, const std::string & outputFilePath, const std::string & searchStr, const std::string & replaceStr)
 {
-	if (argc != 5)
-	{
-		std::cerr << "Invalid params. Use this:" << std::endl;
-		std::cerr << "replace.exe <input file> <output file> <search string> <replace string>" << std::endl;
-		return 1;
-	}
-
-	std::ifstream input(argv[1]);
+	std::ifstream input(inputFilePath);
 	if (!input.is_open())
 	{
-		std::cerr << "Failed to open " << argv[1] << " for reading" << std::endl; 
-		return 1;
+		throw std::runtime_error("Failed to open " + inputFilePath + " for reading");
 	}
 
-	std::ofstream output(argv[2]);
+	std::ofstream output(outputFilePath);
 	if (!output.is_open())
 	{
-		std::cerr << "Failed to open " << argv[2] << " for writing" << std::endl;
-		return 1;
-	}
-
-	const std::string searchStr = argv[3];
-	const std::string replaceStr = argv[4];
-	if (searchStr.empty())
-	{
-		std::cerr << "<search string> must be not empty" << std::endl;
-		return 1;
+		throw std::runtime_error("Failed to open " + outputFilePath + " for writing");
 	}
 
 	std::string currentStr;
@@ -61,8 +42,40 @@ int main(int argc, char * argv[])
 
 	if (!output.flush())
 	{
-		std::cerr << "Failed to save data on disk" << std::endl;
+		throw std::runtime_error("Failed to save data on disk");
+	}
+}
+
+}
+
+int main(int argc, char * argv[])
+{
+	if (argc != 5)
+	{
+		std::cerr << "Invalid params. Use this:" << std::endl;
+		std::cerr << "replace.exe <input file> <output file> <search string> <replace string>" << std::endl;
 		return 1;
 	}
+
+	const std::string inputFilePath = argv[1];
+	const std::string outputFilePath = argv[2];
+	const std::string searchStr = argv[3];
+	const std::string replaceStr = argv[4];
+	if (searchStr.empty())
+	{
+		std::cerr << "<search string> must be not empty" << std::endl;
+		return 1;
+	}
+
+	try
+	{
+		Replace(inputFilePath, outputFilePath, searchStr, replaceStr);
+	}
+	catch (const std::exception & exception)
+	{
+		std::cerr << exception.what() << std::endl;
+		return 1;
+	}
+
 	return 0;
 }
