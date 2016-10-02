a1745f1cc88971b1b34def98c9eaf343a98f5275
eligantRU <eligant.ru@gmail.com>
2016-10-02 14:45:08 +0400

    Now tests for the crypt with /B key

--- lw1/crypt/test.bat
+++ lw1/crypt/test.bat
@@ -5,7 +5,7 @@ set PROGRAM="%~1"
 REM No params
 CALL %PROGRAM% 2>%TEMP%\err.txt
 IF NOT ERRORLEVEL 0 GOTO errs
-FC %TEMP%\err.txt correctTest\correctNoParams.txt
+FC /B %TEMP%\err.txt correctTest\correctNoParams.txt
 IF ERRORLEVEL 1 GOTO errs
 DEL %TEMP%\err.txt
 ECHO Test "no params" SUCCEED 
@@ -13,7 +13,7 @@ ECHO Test "no params" SUCCEED
 REM Invalid <mode>
 CALL %PROGRAM% bla inputTest\empty.txt %TEMP%\out.txt 0 2>%TEMP%\err.txt
 IF NOT ERRORLEVEL 0 GOTO errs
-FC %TEMP%\err.txt correctTest\correctInvalidMode.txt
+FC /B %TEMP%\err.txt correctTest\correctInvalidMode.txt
 IF ERRORLEVEL 1 GOTO errs
 DEL %TEMP%\err.txt
 DEL %TEMP%\out.txt
@@ -22,7 +22,7 @@ ECHO Test "Invalid <mode>" SUCCEED
 REM <key> = -1
 CALL %PROGRAM% crypt inputTest\empty.txt %TEMP%\out.txt -1 2>%TEMP%\err.txt
 IF NOT ERRORLEVEL 0 GOTO errs
-FC %TEMP%\err.txt correctTest\correctKey-1.txt
+FC /B %TEMP%\err.txt correctTest\correctKey-1.txt
 IF ERRORLEVEL 1 GOTO errs
 DEL %TEMP%\err.txt
 DEL %TEMP%\out.txt
@@ -31,7 +31,7 @@ ECHO Test "<key> = -1" SUCCEED
 REM <key> = 270
 CALL %PROGRAM% crypt inputTest\empty.txt %TEMP%\out.txt 270 2>%TEMP%\err.txt
 IF NOT ERRORLEVEL 0 GOTO errs
-FC %TEMP%\err.txt correctTest\correctKey270.txt
+FC /B %TEMP%\err.txt correctTest\correctKey270.txt
 IF ERRORLEVEL 1 GOTO errs
 DEL %TEMP%\err.txt
 DEL %TEMP%\out.txt
@@ -40,7 +40,7 @@ ECHO Test "<key> = 270" SUCCEED
 REM <key> = 'F'
 CALL %PROGRAM% crypt inputTest\empty.txt %TEMP%\out.txt F 2>%TEMP%\err.txt
 IF NOT ERRORLEVEL 0 GOTO errs
-FC %TEMP%\err.txt correctTest\correctKeyF.txt
+FC /B %TEMP%\err.txt correctTest\correctKeyF.txt
 IF ERRORLEVEL 1 GOTO errs
 DEL %TEMP%\err.txt
 DEL %TEMP%\out.txt
@@ -49,7 +49,7 @@ ECHO Test "<key> = 'F'" SUCCEED
 REM EmptyFile
 CALL %PROGRAM% crypt inputTest\empty.txt %TEMP%\out.txt 255
 IF ERRORLEVEL 1 GOTO errs 
-FC %TEMP%\out.txt correctTest\correctEmpty.txt 
+FC /B %TEMP%\out.txt correctTest\correctEmpty.txt 
 IF ERRORLEVEL 1 GOTO errs 
 DEL %TEMP%\out.txt 
 ECHO Test "EmptyFile" SUCCEED
@@ -57,7 +57,7 @@ ECHO Test "EmptyFile" SUCCEED
 REM OneLine File 104 crypt
 CALL %PROGRAM% crypt inputTest\OneLine104.txt %TEMP%\out.txt 104
 IF ERRORLEVEL 1 GOTO errs 
-FC %TEMP%\out.txt correctTest\correctOneLine104Crypt.txt 
+FC /B %TEMP%\out.txt correctTest\correctOneLine104Crypt.txt 
 IF ERRORLEVEL 1 GOTO errs 
 DEL %TEMP%\out.txt 
 ECHO Test "OneLine File 104 crypt" SUCCEED
@@ -65,7 +65,7 @@ ECHO Test "OneLine File 104 crypt" SUCCEED
 REM OneLine File 104 decrypt
 CALL %PROGRAM% decrypt inputTest\OneLine104Decrypt.txt %TEMP%\out.txt 104
 IF ERRORLEVEL 1 GOTO errs 
-FC %TEMP%\out.txt correctTest\correctOneLine104Decrypt.txt 
+FC /B %TEMP%\out.txt correctTest\correctOneLine104Decrypt.txt 
 IF ERRORLEVEL 1 GOTO errs 
 DEL %TEMP%\out.txt 
 ECHO Test "OneLine File 104 decrypt" SUCCEED
@@ -73,7 +73,7 @@ ECHO Test "OneLine File 104 decrypt" SUCCEED
 REM Multiline File 55 crypt
 CALL %PROGRAM% crypt inputTest\Multiline55.txt %TEMP%\out.txt 55
 IF ERRORLEVEL 1 GOTO errs 
-FC %TEMP%\out.txt correctTest\correctMultiline55Crypt.txt 
+FC /B %TEMP%\out.txt correctTest\correctMultiline55Crypt.txt 
 IF ERRORLEVEL 1 GOTO errs 
 DEL %TEMP%\out.txt 
 ECHO Test "Multiline File 55 crypt" SUCCEED
@@ -81,7 +81,7 @@ ECHO Test "Multiline File 55 crypt" SUCCEED
 REM Multiline File 55 decrypt
 CALL %PROGRAM% decrypt inputTest\Multiline55Decrypt.txt %TEMP%\out.txt 55
 IF ERRORLEVEL 1 GOTO errs 
-FC %TEMP%\out.txt correctTest\correctMultiline55Decrypt.txt 
+FC /B %TEMP%\out.txt correctTest\correctMultiline55Decrypt.txt 
 IF ERRORLEVEL 1 GOTO errs 
 DEL %TEMP%\out.txt 
 ECHO Test "Multiline File 55 decrypt" SUCCEED
