3b1d8fe3555e2cfd8b143f96418b51e4588c8dc4
eligantRU <eligant.ru@gmail.com>
2016-10-10 01:02:17 +0400

    Added auto-tests for the replace

--- lw2/replace_tests/replace_tests.cpp
+++ lw2/replace_tests/replace_tests.cpp
@@ -4,9 +4,49 @@
 
 BOOST_AUTO_TEST_SUITE(Test)
 
-BOOST_AUTO_TEST_CASE(_)
+BOOST_AUTO_TEST_CASE(Find_And_Replace)
 {
-	BOOST_CHECK(true);
+	{
+		const std::string expectedResult = "Hello world!";
+		auto result = FindAndReplace("Hello world!", "world", "world");
+		BOOST_CHECK(result == expectedResult);
+	}
+
+	{
+		const std::string expectedResult = "Hello elephant!";
+		auto result = FindAndReplace("Hello world!", "world", "elephant");
+		BOOST_CHECK(result == expectedResult);
+	}
+
+	{
+		const std::string expectedResult = "Hello C++!";
+		auto result = FindAndReplace("Hello world!", "world", "C++");
+		BOOST_CHECK(result == expectedResult);
+	}
+
+	{
+		const std::string expectedResult = "Hello boost boost boost boost!";
+		auto result = FindAndReplace("Hello world world world world!", "world", "boost");
+		BOOST_CHECK(result == expectedResult);
+	}
+
+	{
+		const std::string expectedResult = "Hello    bla-bla-bla !";
+		auto result = FindAndReplace("Hello world world world bla-bla-bla world!", "world", "");
+		BOOST_CHECK(result == expectedResult);
+	}
+
+	{
+		const std::string expectedResult = "Hello completed HELLO hElLo";
+		auto result = FindAndReplace("Hello hello HELLO hElLo", "hello", "completed");
+		BOOST_CHECK(result == expectedResult);
+	}
+
+	{
+		const std::string expectedResult = "01234567890123456789";
+		auto result = FindAndReplace("0123456789", "0", "01234567890");
+		BOOST_CHECK(result == expectedResult);
+	}
 }
 
 BOOST_AUTO_TEST_SUITE_END()
