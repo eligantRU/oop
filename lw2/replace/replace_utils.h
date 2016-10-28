04334933f3ed04fa3ac4055d05ebcf521b81de8d
eligantRU <eligant.ru@gmail.com>
2016-10-29 00:10:00 +0400

    Refactoring(FindAndReplace)
    Signed-off-by: eligantRU <eligant.ru@gmail.com>

--- lw2/replace/replace_utils.h
+++ lw2/replace/replace_utils.h
@@ -3,4 +3,5 @@
 #include "stdafx.h"
 
 std::string FindAndReplace(const std::string & inputStr, const std::string & searchStr, const std::string & replaceStr);
+
 void Replace(const std::string & inputFilePath, const std::string & outputFilePath, const std::string & searchStr, const std::string & replaceStr);
