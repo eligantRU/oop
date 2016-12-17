f12f6deac8f097812db17ca895540993ec54bb7d
eligantRU <eligant.ru@gmail.com>
2016-12-17 08:42:19 +0400

    The code was brushed

--- lw7/FindMaxEx_tests/findMaxEx_tests.cpp
+++ lw7/FindMaxEx_tests/findMaxEx_tests.cpp
@@ -22,16 +22,16 @@ bool IsLessHeight(const SAthlete & lhs, const SAthlete & rhs)
 	return (lhs.height < rhs.height);
 }
 
-SAthlete Tigress = { "Master Tigress", 220.5, 100.50 };
-SAthlete Monkey = { "Master Monkey ", 150.8, 78.42 };
-SAthlete Mantis = { "Master Mantis", 18.5, 0.85 };
-SAthlete Crane = { "Master Crane", 180.0, 50.20 };
-SAthlete Viper = { "Master Viper", 250.5, 42.05 };
-SAthlete Po = { "Dragon Warrior", 187.96, 118.00 };
+const SAthlete Tigress = { "Master Tigress", 220.5, 100.50 };
+const SAthlete Monkey = { "Master Monkey ", 150.8, 78.42 };
+const SAthlete Mantis = { "Master Mantis", 18.5, 0.85 };
+const SAthlete Crane = { "Master Crane", 180.0, 50.20 };
+const SAthlete Viper = { "Master Viper", 250.5, 42.05 };
+const SAthlete Po = { "Dragon Warrior", 187.96, 118.00 };
 
 const std::vector<SAthlete> athletes = { Tigress, Monkey, Mantis, Crane, Viper, Po };
 
-void IsAthleteEqual(const SAthlete & lhs, const SAthlete & rhs)
+void VerifyAthlete(const SAthlete & lhs, const SAthlete & rhs)
 {
 	BOOST_CHECK_EQUAL(lhs.name, rhs.name);
 	BOOST_CHECK_EQUAL(lhs.height, rhs.height);
@@ -55,13 +55,13 @@ BOOST_AUTO_TEST_SUITE(FindMax_)
 		{
 			auto lessAthlete = Tigress;
 			BOOST_CHECK(!FindMax({ }, lessAthlete, IsLessWeight));
-			IsAthleteEqual(lessAthlete, Tigress);
+			VerifyAthlete(lessAthlete, Tigress);
 		}
 
 		{
 			auto lessAthlete = Po;
 			BOOST_CHECK(!FindMax({ }, lessAthlete, IsLessHeight));
-			IsAthleteEqual(lessAthlete, Po);
+			VerifyAthlete(lessAthlete, Po);
 		}
 	}
 
@@ -69,22 +69,22 @@ BOOST_AUTO_TEST_SUITE(FindMax_)
 	{
 		{
 			int max;
-			BOOST_CHECK(FindMax({ 16, 9, 11, 3, 17, 4, 0 }, max, [](const auto lhs, const auto rhs) {
+			BOOST_CHECK(FindMax({ -16, -9, -11, -3, -17, -4 }, max, [](const auto lhs, const auto rhs) {
 				return (lhs < rhs);
 			}));
-			BOOST_CHECK_EQUAL(max, 17);
+			BOOST_CHECK_EQUAL(max, -3);
 		}
 
 		{
 			SAthlete theWeightestAthlete;
 			BOOST_CHECK(FindMax(athletes, theWeightestAthlete, IsLessWeight));
-			IsAthleteEqual(theWeightestAthlete, Po);
+			VerifyAthlete(theWeightestAthlete, Po);
 		}
 
 		{
 			SAthlete theHeightestAthlete;
 			BOOST_CHECK(FindMax(athletes, theHeightestAthlete, IsLessHeight));
-			IsAthleteEqual(theHeightestAthlete, Viper);
+			VerifyAthlete(theHeightestAthlete, Viper);
 		}
 	}
 
